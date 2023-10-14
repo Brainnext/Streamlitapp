25a0ef350703417d824fc039d677e5cc644c1d17
Sirlinux <ainaemanuel@gmail.com>
2023-10-14 02:06:36 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({ 'first' : [1,2,3,4,5] , 
-                    'second' : [4,5,6,3,3], 'third': [4,2,2,4,6], 'fourth': [2,3,0,1,2])}
+                    'second' : [4,5,6,3,3], 'third': [4,2,2,4,6], 'fourth': [2,3,0,1,2]})
 
 df
 
