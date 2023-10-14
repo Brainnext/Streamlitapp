5646bce6ff8a5303a8068110d904a32c61ad31ad
Sirlinux <ainaemanuel@gmail.com>
2023-10-14 02:06:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({ 'first' : [1,2,3,4,5] , 
-                    'second' : [4,5,6,3,3]})
+                    'second' : [4,5,6,3,3], 'third': [4,2,2,4,6], 'fourth': [2,3,0,1,2])}
 
 df
+
+st.markdown("Thats it for today")
+
