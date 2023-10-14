216e9a67b9e1d962b6028ca32adeea3c41b55ceb
Sirlinux <ainaemanuel@gmail.com>
2023-10-14 02:02:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-# Writing out the title and a paragraph
-st.title("Gba AI")
-st.write("Conventional gba LLM lol. Lets have some fun")
+df = pd.DataFrame({ 'first' : [1,2,3,4,5] , 
+                    'second' : [4,5,6,3,3]})
 
-with st.markdown("Gbaman"):
-  st.write("Welcome to gba land, nigga!")
-
-user_message = st.text_input("You:", "")
-
-if user_message:
-  st.markdown(f"You: {user_message}")
-
-response = "Hello!, This is Gba AI"
-
-st.markdown(f"chatbot: {response}")
+print (df)
