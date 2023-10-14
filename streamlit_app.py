edd43bb51c533927a6c4f898500b9d029ad56c36
Sirlinux <ainaemanuel@gmail.com>
2023-10-14 02:10:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 user_message = st.text_input("Please Enter Any Number")
 
-if (user_message)  = 2000:
+if (user_message)  == 2000:
   st.markdown("Correct answer") & st.Title("Nice!!!")
 else:
   st.markdown("Try again!")
