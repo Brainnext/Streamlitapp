a4f93039448b7fafebbc61b3847a2ee854c77412
Sirlinux <ainaemanuel@gmail.com>
2023-10-17 17:27:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import pandas as pd
 import streamlit as st
 
+st.title("Welcome to streamlit")
 st.write("Hello, World!")
+st.subheader("Hey, man")
+st.markdown("Nice to have you here")
+
+st.image("https://unsplash.com/photos/WCUQImjKO1s")
