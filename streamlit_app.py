4be93dc579ae84f0c2633dd809213ad7297d0df3
Sirlinux <ainaemanuel@gmail.com>
2023-10-17 17:34:12 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ st.write("Hello, World!")
 st.subheader("Hey, man")
 st.markdown("Nice to have you here")
 
-st.video("https://youtu.be/OVCvXFDQdVc?si=U5yYStoZiQDnO4tQ")
+st.subheader("Watch this Karma video by Seyi Vibez")
+st.video("https://youtu.be/dN4QxTcis2w?si=bX-CpK3fh3SjSzmQ")
+st.caption("Karma - Seyi Vibez")
