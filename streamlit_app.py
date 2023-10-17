e4d411d1032aca7a2296adc51fe8a79e723ab3e1
Sirlinux <ainaemanuel@gmail.com>
2023-10-17 17:08:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-user_message = st.text_input("Please Enter Any Number")
-
-if (user_message)  == 2000:
-  st.markdown("Correct answer") & st.Title("Nice!!!")
-else:
-  st.markdown("Try again!")
-  
+st.title("Welcome to my machine learning")
+st.chat_message("Welcome")
